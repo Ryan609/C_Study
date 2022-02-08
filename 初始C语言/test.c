44722ae95d20dd48d55f22b0783f9eecb3afb1c6
Ryan609 <8380@88.com>
2022-02-08 10:55:32 +1030

    first

---
+++ 初始C语言/test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// int main() {
+//     // printf("Hello World\n");
+//     // printf("hehe\n");
+
+//     printf("%lu\n", sizeof(char));
+//     printf("%lu\n", sizeof(short));
+//     printf("%lu\n", sizeof(int));
+//     printf("%lu\n", sizeof(long));
+//     printf("%lu\n", sizeof(float));
+// }
+//------------------------------------------------
+
+// int b = 1000; //全局变量 - 定义在大括号的外部
+// int main() {
+// 	int a = 100; //局部变量 - 定义在大括号的内部
+//     printf("%d\n", a);
+//     printf("%d\n", b);
+// }
+//------------------------------------------------
+
+int main(){
+    int num1 = 0;
+    int num2 = 0;
+    int sum = 0;
+
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("%d\n", sum);
+}
