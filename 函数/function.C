b17366aba2e34ab7be3f57425436672839e9e040
Ryan609 <8380@88.com>
2022-02-17 16:14:38 +1030

    2

--- 函数/function.C
+++ 函数/function.C
@@ -1,20 +1,37 @@
 #include <stdio.h>
 //--------------------------------函数递归
-void print(int n)
-{
-    if (n > 9)
-    {
-        print(n/10);
-    }
-    printf("%d ", n%10);
+// void print(int n)
+// {
+//     if (n > 9)
+//     {
+//         print(n/10);
+//     }
+//     printf("%d ", n%10);
     
-}
+// }
 
-int main() 
-{   
-    unsigned int num = 0;
-    scanf("%d", &num);
-    print(num);
-    return 0;
-}
-//--------------------------------
+// int main() 
+// {   
+//     unsigned int num = 0;
+//     scanf("%d", &num);
+//     print(num);
+//     return 0;
+// }
+//--------------------------------n的阶乘
+// int fac(int n)
+// {
+//     if (n < 1)
+//         return 1;
+//     else
+//         return n * fac(n - 1);
+// }
+
+// int main()
+// {
+//     int n = 0;
+//     scanf("%d", &n);
+//     int ret = fac(n);
+//     printf("%d\n", ret);
+//     return 0;
+// }
+//--------------------------------求第n个斐波那契数
