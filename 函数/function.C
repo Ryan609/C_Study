ee6ea0a189e17dede73d1801bbdef6a1555452d2
Ryan609 <8380@88.com>
2022-02-17 16:54:32 +1030

    3

--- 函数/function.C
+++ 函数/function.C
@@ -35,3 +35,35 @@
 //     return 0;
 // }
 //--------------------------------求第n个斐波那契数
+// int fib(int n) //递归浪费时间，需要大量计算，递归不是好的办法
+// {
+//     if (n <= 2)
+//         return 1;
+//     else
+//         return fib(n - 1)+ fib(n-2);
+// }
+
+int fib(int n) //递归浪费时间，需要大量计算，递归不是好的办法
+{
+    int a = 1;
+    int b = 1;
+    int c = 1;
+    while (n > 2)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        n--;
+    }
+    return c;
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int ret = fib(n);
+    printf("%d\n", ret);
+    return 0;
+}
+//--------------------------------
