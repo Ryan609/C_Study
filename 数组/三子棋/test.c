4221ac5e28e321fde1dc15abf30256d1d143bb11
Ryan609 <8380@88.com>
2022-02-20 21:16:48 +1030

    三子棋完整版

--- 数组/三子棋/test.c
+++ 数组/三子棋/test.c
@@ -9,18 +9,42 @@ void menu() {
 }
 
 void game() {
-	
+	char ret = 0;
 	char board[ROW][COL] = { 0 }; //存放下棋的数据
+	
 	InitBoard(board, ROW, COL); //初始化棋盘
 	DisplayBoard(board, ROW, COL); //打印棋盘
 	
-	Player_move(board, ROW, COL); //玩家下棋
-	DisplayBoard(board, ROW, COL);
+	while (1) {
+		Player_move(board, ROW, COL); //玩家下棋
+		DisplayBoard(board, ROW, COL); 
+		ret = is_win(board, ROW, COL);
+		if (ret != 'C') {
+			break;
+		}
+		
+		computer_move(board, ROW, COL);//电脑下棋
+		DisplayBoard(board, ROW, COL);
+		ret = is_win(board, ROW, COL);
+		if (ret != 'C') {
+			break;
+		}
+	}
 	
+	if (ret == '*') {
+		printf("玩家赢了\n");
+	} else if (ret == '#') {
+		printf("电脑赢了\n");
+	} else {
+		printf("平局\n");
+	}
+	//DisplayBoard(board, ROW, COL);
+
 }
 
 void test() {
 	int input = 0;
+	srand((unsigned int)time(NULL));
 	
 	do {
 		menu();
