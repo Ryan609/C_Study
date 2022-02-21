97252946b5f605f6af4591851210ffcaa4d48200
Ryan609 <8380@88.com>
2022-02-21 21:41:43 +1030

    扫雷final

--- 数组/扫雷/test.c
+++ 数组/扫雷/test.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+
+#include "game.h"
 
 void menu() {
 	printf("*************************\n");
@@ -8,12 +9,30 @@ void menu() {
 }
 
 void game() {
+	//扫雷游戏的实现
+	char mine[ROWS][COLS] = { 0 };
+	char show[ROWS][COLS] = { 0 };
+	
+	//初始化棋盘
+	init_board(mine, ROWS, COLS, '0');
+	init_board(show, ROWS, COLS, '*');
+	
+	//打印棋盘
+	
+	//show_board(show, ROW, COL);
+	
+	//布置雷
+	set_mine(mine, ROW, COL);
+	show_board(show, ROW, COL);
+	
+	//排查雷
+	find_mine(mine,show,ROW,COL);
 	
 }
 
 int main() {
 	int input = 0;
-	
+	srand((unsigned int)time(NULL));
 	do {
 		menu();
 		printf("请选择：");
@@ -32,3 +51,4 @@ int main() {
 		
 	} while (input);
 }
+
