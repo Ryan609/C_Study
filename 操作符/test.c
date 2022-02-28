679ee21f05444cc5e10005f8c1ba756a6ccd81be
Ryan609 <8380@88.com>
2022-02-28 19:40:14 +1030

    123

--- 操作符/test.c
+++ 操作符/test.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 //不创建临时变量，实现两个数的交换
+//int main() {
+//	int a = 3;
+//	int b = 5;
+//	printf("a=%d b=%d\n", a, b);
+//	
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d b=%d\n", a, b);
+//	
+//	return 0;
+//}
 int main() {
-	int a = 3;
-	int b = 5;
-	printf("a=%d b=%d\n", a, b);
-	
-	a = a ^ b;
-	b = a ^ b;
-	a = a ^ b;
-	printf("a=%d b=%d\n", a, b);
-	
-	return 0;
+	int a = 0x11223344;
+	int* pa = &a;
+	*pa = 0;
 }
