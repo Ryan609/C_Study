e4ec868a8f0c3616969211c7e0050371a8690c96
Ryan609 <8380@88.com>
2022-03-03 10:05:43 +1030

    1

--- 操作符/test.c
+++ 操作符/test.c
@@ -25,14 +25,12 @@ int Add(int x, int y)
 	z = x + y;
 	return z;
 }
-
-int main()
-{
-	int a = 10;
+int main() {
+		int a = 10;
 	int b = 20;
 	int c = 0;
 	c = Add(a, b);
 	printf("%d\n", c);
 
 	return 0;
-}
+};
