b6ea782f406356c2d21c8cd4f49539f943304430
Ryan609 <8380@88.com>
2022-03-04 20:26:50 +1030

    123

---
+++ 调试/test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//int main() {
+//	int n = 0;
+//	int i = 0;
+//	int sum = 0;
+//	int ret = 1;
+//	
+//	for (n = 1; n <= 3; n++) {
+//		ret *= n;
+//		sum += ret;
+//	}
+//	
+//	printf("%d\n", sum);
+//	
+//	return 0;
+//}
+
+int main() {
+	int i = 0;
+	int arr[10] = {0};
+	for(i=0; i<=10; i++) {
+			arr[i] = 0;
+			printf("hehe\n");
+	}
+	return 0; 
+}
